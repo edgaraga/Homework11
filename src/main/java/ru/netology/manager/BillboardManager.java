8c990cfff8ea0b1e6eea3b510801a25a13569f05
edgaraga <102522815+edgaraga@users.noreply.github.com>
2022-06-10 14:36:25 +0500

    Update BillboardManager.java
    Fixed build error

--- src/main/java/ru/netology/manager/BillboardManager.java
+++ src/main/java/ru/netology/manager/BillboardManager.java
@@ -16,18 +16,6 @@ public class BillboardManager {
         ViewingItem[] items = repository.findAll();
         ViewingItem[] result = new ViewingItem[items.length];
         for (int i = 0; i < items.length; i++) {
-
-    public ViewingItem[] findLast() {
-
-        int resultLength;
-        if (limitManager == items.length) {
-            resultLength = items.length;
-        } else {
-            resultLength = limitManager;
-        }
-        ViewingItem[] result = new ViewingItem[resultLength];
-        for (int i = 0; i < resultLength; i++) {
-
             int index = items.length - i - 1;
             result[i] = items[index];
         }
